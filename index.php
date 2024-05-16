5e424c4ff8d3b469c75e70f9d9e5b264aa7fe507
Duckaert Dennis (s149322) <dennis.duckaert@student.ap.be>
2024-05-16 21:09:10 +0200

    test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <?php
     echo "<h1>PHP Testpagina333</h1>";
-    echo "<p>Dit is een testpagina om te controleren of de git workflow werkt</p>";
+    echo "<p>Dit is een testpagina om te controleren of de git</p>";
     echo "<p>De huidige datum en tijd is: " . date("Y-m-d H:i:s") . "</p>";
 ?>
     
